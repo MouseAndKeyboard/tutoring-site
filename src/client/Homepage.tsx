f4073be3ea5d84aa33b57b1c45a3aab7c9e344cc
M Nef <howtoeatpineapples@gmail.com>
2020-02-15 01:07:05 +0800

    redesigned site navbar to be nicer

--- src/client/Homepage.tsx
+++ src/client/Homepage.tsx
@@ -1,17 +1,30 @@
-import React, {useState, useEffect, Fragment} from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 
-import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container';
 
 const Homepage = () => {
     return (
-        <Fragment>
-            <h1>
-                Tutoring gang
-            </h1>
-            <Button>
-                test
-            </Button>
-        </Fragment>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container >
+                <Navbar.Brand href="#home">BPhil Tutor Gang</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#hire">Hire a Tutor</Nav.Link>
+                        <Nav.Link href="#apply">Become a Tutor</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#about">About us</Nav.Link>
+                        <Nav.Link href="#contact">Contact us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
