888a80f2f2dec0e77775e46090bc9a0d5dedc0cf
M Nef <howtoeatpineapples@gmail.com>
2020-02-15 00:04:19 +0800

    installed react-bootstrap

--- src/client/Homepage.tsx
+++ src/client/Homepage.tsx
@@ -1,11 +1,16 @@
 import React, {useState, useEffect, Fragment} from 'react'
 
+import Button from 'react-bootstrap/Button';
+
 const Homepage = () => {
     return (
         <Fragment>
             <h1>
                 Tutoring gang
             </h1>
+            <Button>
+                test
+            </Button>
         </Fragment>
     );
 }
