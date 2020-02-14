dee8169033b7efc521530b3eecd6762c0a64f6e0
M Nef <howtoeatpineapples@gmail.com>
2020-02-15 01:10:40 +0800

    moved navigation bar into its own file

--- src/client/Homepage.tsx
+++ src/client/Homepage.tsx
@@ -1,30 +1,16 @@
 import React, { useState, useEffect, Fragment } from 'react'
-
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container';
+import Navigation from './Navbar';
 
 const Homepage = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container >
-                <Navbar.Brand href="#home">BPhil Tutor Gang</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#hire">Hire a Tutor</Nav.Link>
-                        <Nav.Link href="#apply">Become a Tutor</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#about">About us</Nav.Link>
-                        <Nav.Link href="#contact">Contact us</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Fragment>
+            <Navigation />
+
+            <h1>
+                test
+            </h1>
+        </Fragment>
+        
     );
 }
 
---
+++ src/client/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+
+const Navigation = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container >
+                <Navbar.Brand href="#home">BPhil Tutor Gang</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#hire">Hire a Tutor</Nav.Link>
+                        <Nav.Link href="#apply">Become a Tutor</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#about">About us</Nav.Link>
+                        <Nav.Link href="#contact">Contact us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Navigation;
