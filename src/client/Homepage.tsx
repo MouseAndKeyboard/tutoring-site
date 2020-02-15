1c15c826568024c880ccef985ceec74f6bfc33b3
M Nef <howtoeatpineapples@gmail.com>
2020-02-15 11:39:51 +0800

    added typist to home page

--- src/client/Homepage.tsx
+++ src/client/Homepage.tsx
@@ -1,14 +1,34 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import Navigation from './Navbar';
+import { Container, Jumbotron, Carousel, CarouselItem } from 'react-bootstrap';
+
+import Typist from 'react-typist';
+
+import TutorProfileCard from './TutorProfileCard';
 
 const Homepage = () => {
+
+    let onHeaderTyped = () => {
+
+    }
+
     return (
         <Fragment>
             <Navigation />
 
-            <h1>
-                test
-            </h1>
+            <Jumbotron>
+                <Container>
+                    <div className="d-flex">
+                        <span className="title px-2">We are: </span>
+                        <Typist 
+                            className="title"
+                            >
+                            students
+                        </Typist>
+                    </div>
+                    
+                </Container>
+            </Jumbotron>
         </Fragment>
         
     );
